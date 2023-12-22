95e8a91077f0b88644eed0bea7b55066a9a0664e
Valentin Giorgetti <giorgettivalentin@gmail.com>
2023-12-22 14:33:34 +0100

    adding comment and fixing method names

--- customer-utils.js
+++ customer-utils.js
@@ -1,12 +1,30 @@
-const sortByCountry = (customers, country) => {
+/**
+ * Filter customers by country using built-in Array.filter
+ * @param {Array} customers - Array of customers
+ * @param {string} country - Country to filter by
+ * @returns {Array} - Filtered array of customers
+ */
+const filterByCountry = (customers, country) => {
     return customers.filter((customer) => customer.country === country);
 }
 
+/**
+ * Get the country code from the country name
+ * @param {string} country - Country name
+ * @param {Object} countries - Object with country codes and names as key-value pairs
+ * @returns {string} - Country code
+ */
 const getCountryCode = (country, countries) => {
     const countryCode = Object.keys(countries).find((key) => countries[key] === country);
     return countryCode;
 }
 
+/**
+ * Add country code to each customer object
+ * @param {Array} customers - Array of customers
+ * @param {Object} countries - Object with country codes and names as key-value pairs
+ * @returns {Array} - Array of customers with country code
+ */
 const addCountryCode = (customers, countries) => {
     return customers.map((customer) => {
         return {
@@ -16,6 +34,12 @@ const addCountryCode = (customers, countries) => {
     });
 }
 
+/**
+ * Create Stripe customers
+ * @param {Array} customers - Array of customers
+ * @param {Object} stripeInstance - Stripe instance
+ * @returns {Array} - Array of customers added to Stripe
+ */
 const createStripeCustomers = async (customers, stripeInstance) => {
     const stripeCustomers = [];
     for (const customer of customers) {
@@ -36,7 +60,7 @@ const createStripeCustomers = async (customers, stripeInstance) => {
 }
 
 module.exports = {
-    sortByCountry,
+    filterByCountry,
     addCountryCode,
     createStripeCustomers,
 };
--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const stripe = Stripe(STRIPE_TEST_SECRET_KEY);
 
 const CustomerUtils = require("./customer-utils");
 
+/**
+ * Open a JSON file and return a promise with the parsed data
+ * @param {string} path - Path to the JSON file
+ * @returns {Promise<any>} - Promise with the parsed data
+*/
 const openJsonFile = async (path) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, "utf8", (err, data) => {
@@ -15,6 +20,12 @@ const openJsonFile = async (path) => {
   });
 };
 
+/**
+ * Save a JSON file with the given data
+ * @param {string} path - Path to the JSON file
+ * @param {any} data - Data to save
+ * @returns {Promise<void>} - Promise
+ */
 const saveJsonToFile = async (path, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(path, JSON.stringify(data), (err) => {
@@ -29,11 +40,19 @@ const handler = async (country) => {
     let finalCustomers = [];
 
     let customers = await openJsonFile("./customers.json");
-    customers = CustomerUtils.sortByCountry(customers, country);
+    // Using a utility function that retrieves customers from a specific country
+    customers = CustomerUtils.filterByCountry(customers, country);
+
+    // A simple check to see if there are customers from the given country
+    if(customers.length === 0) throw new Error("No customers found for the given country");
 
     const countries = await openJsonFile("./countries-ISO3166.json");
+    // Using a utility function that adds the country code to each customer object
     customers = CustomerUtils.addCountryCode(customers, countries);
 
+    /* This function creates Stripe customers and returns an array of customers added to Stripe
+       It uses the customer object from the previous step and the Stripe instance
+    */
     finalCustomers = await CustomerUtils.createStripeCustomers(
       customers,
       stripe
@@ -47,6 +66,7 @@ const handler = async (country) => {
 };
 
 (async () => {
+  // Slice the first two arguments because they are node and index.js (in most cases)
   const argv = process.argv.slice(2);
   const argc = argv.length;
   if (argc < 1) {

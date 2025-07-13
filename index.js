2c8d31c8bc7ff2c42a517743d1ad0bdf66c20940
Tsilavina Rakotomavo <tsilavinarakotomavo2002@gmail.com>
2025-07-13 22:53:26 +0300

    Apply utility methods in index.js

--- index.js
+++ index.js
@@ -2,40 +2,40 @@ const fs = require('fs')
 const Stripe = require('stripe');
 const STRIPE_TEST_SECRET_KEY = 'sk_test_51MEuPXA69JWLHl3Jxw3gKWTtXJCOkzmvjDs5oJ45DZEHFzo5HLz5JfWkNvzU03eCyo0ojkiW2ot6WXA8udWEkh0300nAnoJmcj'
 const stripe = Stripe(STRIPE_TEST_SECRET_KEY);
+const {
+  readJsonFile,
+  writeJsonFile,
+  filterCustomersByCountry,
+  transformCustomers,
+  createStripeCustomers
+} = require('./utils');
 
 const handler = async (country) => {
-
-  try{
+  try {
     let finalCustomers = []
 
-    /* add code below this line */
-    
     // filter the customers by country
+    const customers = await readJsonFile('customers.json');
+    const filteredCustomers = filterCustomersByCountry(customers, country);
+
     // transform customers to save into Stripe
+    const countryMapping = await readJsonFile('countries-ISO3166.json');
+    const transformedCustomers = transformCustomers(filteredCustomers, countryMapping);
+
     // for each customer create a Stripe customer
+    const stripeCustomers = await createStripeCustomers(stripe, transformedCustomers);
+
     // push into finalCustomers the stripe customers with email, country and id as properties.
+    finalCustomers.push(...stripeCustomers);
+
     // write finalCustomers array into final-customers.json using fs
-    /* 
-      finalCustomers array should look like:
-      finalCustomers = [{
-          email: test@test.com
-          customerId: 1d833d-12390sa-9asd0a2-asdas,
-          country: 'ES'
-        },
-        {
-          email: test@test.com
-          customerId: 1d833d-12390sa-9asd0a2-asdas,
-          country: 'ES'
-        }
-      }] 
-    */
+    writeJsonFile('final-customers.json', finalCustomers);
 
-    /* add code above this line */
-
+    // log the finalCustomers array
     console.log(finalCustomers)
-
-}catch(e){
-  throw e
+  } catch (e) {
+    throw e;
+  }
 }
- 
-} 
+
+handler('Spain');
